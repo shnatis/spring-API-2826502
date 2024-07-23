36867f2a0db95c1ef7304fb7421c0052d563e646
Your Name <you@example.com>
2024-07-23 15:39:36 -0500

    configuracion de bd

--- src/main/java/org/productos/spring/java/productos2826502/entities/Product.java
+++ src/main/java/org/productos/spring/java/productos2826502/entities/Product.java
@@ -1,11 +1,17 @@
 package org.productos.spring.java.productos2826502.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
+@Entity
+@Table(name='products')
+
+
 
 public class Product {
         private Long id;
